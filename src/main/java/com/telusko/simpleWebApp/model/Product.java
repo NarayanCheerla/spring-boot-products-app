9f36428dc4ce06cf5c4badcecde7cba24f01eaba
CNR <narayana.cheerla@gmail.com>
2024-07-16 12:52:14 +0530

    rest integration

--- src/main/java/com/telusko/simpleWebApp/model/Product.java
+++ src/main/java/com/telusko/simpleWebApp/model/Product.java
@@ -1,15 +1,26 @@
 package com.telusko.simpleWebApp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
+@Entity
 @Data
-@AllArgsConstructor
 public class Product {
 
+    @Id
     private int prodId;
     private String prodName;
     private int prodPrice;
 
-
+    @Override
+    public String toString() {
+        return "Product{" +
+                "prodId=" + prodId +
+                ", prodName='" + prodName + '\'' +
+                ", prodPrice=" + prodPrice +
+                '}';
+    }
 }
--- src/main/java/com/telusko/simpleWebApp/service/ProductService.java
+++ src/main/java/com/telusko/simpleWebApp/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.telusko.simpleWebApp.service;
 
 import com.telusko.simpleWebApp.model.Product;
+import com.telusko.simpleWebApp.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -9,9 +11,28 @@ import java.util.List;
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(new Product(101, "IPhone", 50000), new Product(102, "Samsung", 70000), new Product(103, "Nokia", 12000));
+//    List<Product> products = Arrays.asList(new Product(101, "IPhone", 50000), new Product(102, "Samsung", 70000), new Product(103, "Nokia", 12000));
+
+    @Autowired
+    ProductRepo repo;
 
     public List<Product> getProducts(){
-        return products;
+        return repo.findAll();
+    }
+
+    public void saveProduct(Product product){
+        repo.save(product);
+    }
+
+    public Product getProduct(int id){
+        return repo.findById(id).get();
+    }
+
+    public void deleteProduct(int id){
+        repo.deleteById(id);
+    }
+
+    public Product updateProduct(Product product){
+        return repo.save(product);
     }
 }
